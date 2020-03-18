af89929968331f4b9961722c903b18407b8d1711
Anton Voitenko <voytenko.ad@yandex.ru>
2020-03-18 20:59:57 +0800

    Fix bug with sending cookies when login user

--- controllers/users.js
+++ controllers/users.js
@@ -20,15 +20,13 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then(user => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: true
-        })
-        .end();
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true
+      });
+      res.status(200).send({ message: AUTHORIZATION });
     })
-    .then(res.status(200).send({ message: AUTHORIZATION }))
     .catch(next);
 };
 
