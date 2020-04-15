39de01f753d931689010b91892083f8208e14c19
Anton Voitenko <voytenko.ad@yandex.ru>
2020-04-15 14:45:34 +0800

    Disabled sameSite

--- controllers/users.js
+++ controllers/users.js
@@ -25,7 +25,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true
+        sameSite: false
       });
       res.status(200).send({ name, email });
     })
